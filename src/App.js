f38f7cdc8cb70627545ed8ffe655c723275844f0
Rohan Saluja <rohansalujamusic@gmail.com>
2022-07-09 13:23:10 +0530

    added layout management and created title

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Navs from './components/Navs';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 
@@ -8,20 +7,17 @@ function App() {
   return (
     // <Switch></Switch> works like switch() in Programming
     // Here, we define Cases as paths.
-    <div>
-      <Navs />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/starred">
-          <Starred />
-        </Route>
-        <Route>
-          <div>Not Found</div>
-        </Route>
-      </Switch>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
+      <Route>
+        <div>Not Found</div>
+      </Route>
+    </Switch>
   );
 }
 
