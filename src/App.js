cc1c7a216df8d6ac611b276a9f7792a18b271a47
Rohan Saluja <rohansalujamusic@gmail.com>
2022-07-09 12:59:10 +0530

    added navbar and created page components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
   return (
     // <Switch></Switch> works like switch() in Programming
     // Here, we define Cases as paths.
-    <Switch>
-      <Route exact path="/">
-        This is Home Page
-      </Route>
-      <Route exact path="/starred">
-        This is Starred
-      </Route>
-      <Route>This is 404 Page</Route>
-    </Switch>
+    <div>
+      <Navs />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route>
+          <div>Not Found</div>
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
