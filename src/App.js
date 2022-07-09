4f794cf7f5768703f4155f205cd3d99d0f730674
Rohan Saluja <rohansalujamusic@gmail.com>
2022-07-09 11:45:52 +0530

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>hi</div>;
+  return (
+    // <Switch></Switch> works like switch() in Programming
+    // Here, we define Cases as paths.
+    <Switch>
+      <Route exact path="/">
+        This is Home Page
+      </Route>
+      <Route exact path="/starred">
+        This is Starred
+      </Route>
+      <Route>This is 404 Page</Route>
+    </Switch>
+  );
 }
 
 export default App;
