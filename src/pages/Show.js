030df6afbecbd86c2e8e09a62e9beee989d7e2ff
Rohan Saluja <rohansalujamusic@gmail.com>
2022-07-10 16:17:25 +0530

    replaced useState with useReducer on the show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,13 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS': {
+      return { isLoading: false, error: null, show: action.show };
+    }
+    case 'FETCH_FAILED': {
+      return { ...prevState, isLoading: false, error: action.error };
+    }
+    default:
+      return prevState;
+  }
+};
+
+const initialState = {
+  show: null,
+  isLoading: true,
+  error: null,
+};
 
 const Show = () => {
   const { id } = useParams();
 
-  const [show, SetShow] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [{ show, isLoading, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   useEffect(() => {
     let isMounted = true;
@@ -22,14 +42,12 @@ const Show = () => {
         */
 
         if (isMounted) {
-          SetShow(results);
-          setIsLoading(false);
+          dispatch({ type: 'FETCH_SUCCESS', show: results });
         }
       })
       .catch(err => {
         if (isMounted) {
-          setError(err.message);
-          setIsLoading(false);
+          dispatch({ type: 'FETCH_FAILED', error: err.message });
         }
       });
 
@@ -50,3 +68,25 @@ const Show = () => {
 };
 
 export default Show;
+
+// TODO: ---------------------- Using useState ----------------------------
+
+//   const [show, SetShow] = useState(null);
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+// TODO: ---------------------- Using useState ----------------------------
+
+/*
+        TODO: ----------------------- useState Logic ----------------------------------------
+        if (isMounted) {
+          SetShow(results);
+          setIsLoading(false);
+        }.catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+        TODO: -------------------------------------------------------------------------------
+        */
